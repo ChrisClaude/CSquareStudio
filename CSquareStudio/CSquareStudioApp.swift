8ef5d542c812999e36c3e9bd79f526bc2b1811ea
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-10-28 16:10:44 +0200

    Initial Commit

---
+++ CSquareStudio/CSquareStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSquareStudioApp.swift
+//  CSquareStudio
+//
+//  Created by Claude De-Tchambila on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CSquareStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d32183af91708fd08e4b37bb0120a0f97b7f3321
Chris Claude <christ.tchambila@gmail.com>
2023-11-23 08:02:22 +0100

    Add ImageUploadView.swift

--- CSquareStudio/CSquareStudioApp.swift
+++ CSquareStudio/CSquareStudioApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CSquareStudioApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ImageUploadView()
         }
     }
 }
--- CSquareStudio/ImageUploadView.swift
+++ CSquareStudio/ImageUploadView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct ImageUploadView: View {
+    @State private var isImagePickerPresented: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Image(systemName: "photo.on.rectangle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(Color.red)
+                    .padding()
+                
+                Text("Tap the button below to upload a picture")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                Spacer()
+                
+                Button(action: {
+                    // Trigger image upload logic
+                    self.isImagePickerPresented.toggle()
+                }) {
+                    Text("Upload Picture")
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color.red))
+                        .padding(.horizontal, 20)
+                }
+                .padding(.bottom, 50)
+                .sheet(isPresented: $isImagePickerPresented) {
+                    // Present image picker here
+                    // You can use UIImagePickerController or your custom image picker logic
+                    Text("Image Picker Goes Here")
+                }
+            }
+        }
     }
 }
 

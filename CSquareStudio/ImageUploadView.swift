18bce538f1267b0c727c3231778ab41edc16c3e3
Chris Claude <christ.tchambila@gmail.com>
2023-11-23 08:01:10 +0100

    Add ImageUploadView

---
+++ CSquareStudio/ImageUploadView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageUploadView.swift
+//  CSquareStudio
+//
+//  Created by Claude De-Tchambila on 11/23/23.
+//
+
+import SwiftUI
+
+struct ImageUploadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageUploadView()
+}
